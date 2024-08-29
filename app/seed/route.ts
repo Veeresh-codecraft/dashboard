a753f418cc19b4e4dae9d8b2a5e6fa06826b7304
Veeresh-codecraft <veeresh.km@codecraft.co.in>
2024-08-29 11:34:47 +0530

    implemented revenue in dashboard

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -103,18 +103,17 @@ async function seedRevenue() {
 
 
 
-export async function GET() {
-  try {
-    await client.sql`BEGIN`;
-    await seedUsers();
-    await seedCustomers();
-    await seedInvoices();
-    await seedRevenue();
-    await client.sql`COMMIT`;
-
-    return Response.json({ message: 'Database seeded successfully' });
-  } catch (error) {
-    await client.sql`ROLLBACK`;
-    return Response.json({ error }, { status: 500 });
-  }
+export async function GET() { 
+    try {
+      await client.sql`BEGIN`;
+      await seedUsers();
+      await seedCustomers();
+      await seedInvoices();
+      await seedRevenue();
+      await client.sql`COMMIT`;
+      return Response.json({ message: 'Database seeded successfully' });
+    } catch (error) {
+      await client.sql`ROLLBACK`;
+      return Response.json({ error }, { status: 500 });
+    }
 }
