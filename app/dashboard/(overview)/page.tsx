332c7d8d34ae27b62bac87d4ec8b2eced2528a4c
Veeresh-codecraft <veeresh.km@codecraft.co.in>
2024-08-30 12:57:25 +0530

    jar effect  for user dashboard card component

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,7 +4,7 @@ import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts"; 
 import { fetchCardData } from "@/app/lib/data";
 import { Suspense } from "react";
-import { LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
+import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
 export default async function Page() {
   // const revenue = await fetchRevenue();
   // const latestInvoices = await fetchLatestInvoices(); 
@@ -19,6 +19,7 @@ export default async function Page() {
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
+      <Suspense fallback={<CardsSkeleton/>}>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
@@ -29,6 +30,7 @@ export default async function Page() {
           type="customers"
         />
       </div>
+      </Suspense>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton/>}>
         <RevenueChart />
